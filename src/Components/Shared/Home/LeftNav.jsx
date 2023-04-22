bc6217930e16c16aafda5d900f0416348d5a1cb0
Sarowar-Hossain <mdsarowar1812@gmail.com>
2023-04-22 21:53:11 +0800

    category load from DB

--- src/Components/Shared/Home/LeftNav.jsx
+++ src/Components/Shared/Home/LeftNav.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
-    return (
-        <div>
-            left Navbar
-        </div>
-    );
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  console.log(categories);
+
+  return (
+    <div className="px-4">
+      <h3 className="text-left text-2xl font-bold mb-4">All Caterogy</h3>
+
+      <div className="bg-[#E7E7E7]">
+        <h1 className="text-3xl text-[#403F3F] font-bold py-4">
+          National News
+        </h1>
+      </div>
+      <div className="text-start">
+        {categories.map((categorie) => (
+          <h1
+            className="text-3xl text-[#403F3F] font-bold py-4"
+            key={categorie.id}
+          >
+            <Link to={`/categorie/${categorie.id}`}>{categorie.name}</Link>
+          </h1>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LeftNav;
